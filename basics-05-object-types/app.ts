ce637a8dceb85ef66f14b8c1b65eccc20f076771
HankJoo <wink586@naver.com>
2023-02-27 18:37:58 +0900

    Udemy Study

---
+++ basics-05-object-types/app.ts
@@ -0,0 +1,19 @@
+// type personType = {
+//   name: string;
+//   age: number;
+//   nickname : string;
+// }
+
+const person : {
+  name: string;
+  age: number;
+  nickname : string;
+} = {
+  name: 'HankJoo',
+  age: 30,
+  nickname : '행크'
+};
+
+console.log(person.name);
+console.log(person.nickname);
+console.log(person.age);
