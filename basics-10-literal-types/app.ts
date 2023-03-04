8ac63bc66493862c3dc7868bfc42321ee2f2041d
HankJoo <wink586@naver.com>
2023-03-04 22:18:52 +0900

    function

--- basics-10-literal-types/app.ts
+++ basics-10-literal-types/app.ts
@@ -1,31 +1,22 @@
-//타입 알리어스 /사용자 정의 타입.
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion : ConversionDescriptor
-  // resultConversion: 'as-number' | 'as-text'  //리터럴 타입.
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add (n1:number, n2:number){
+    return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function printResult(num: number) : void {
+    console.log('Result:' + num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+console.log(printResult(add(5, 12)));
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+//let combineValues: Function;  //함수 타입 지정.
+
+let combineValues: (a:number, b:number) => number;
+
+// let combineValues: any;
+
+combineValues = add;
+// combineValues = 5;
+
+console.log(combineValues(8,8));
+
+
